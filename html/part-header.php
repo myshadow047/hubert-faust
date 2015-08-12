dd43a58311defd2608fd3ef526670e8613c226de
aliaraaab <muhammadali.alaydrus@icloud.com>
2015-08-13 01:56:47 +0700

    [Edit] Edit html

--- html/part-header.php
+++ html/part-header.php
@@ -1,39 +1,41 @@
 	<nav class="nav-menu animated slideInDown">
         <div class="container">
-            <div class="pull-left">
-                <h1 class="brand">
-                    <a href="index.php" class="desktop">Hubert &amp; Faust</a>
-                    <a href="index.php" class="mobile">Hubert &amp; Faust</a>
-                </h1>
-                <div class="toggleDown">
-                    <div class="menu-toggle-btn">
-                        <span></span>
-                        <span></span>
-                        <span></span>
+            <div class="row">
+                <div class="pull-left">
+                    <h1 class="brand">
+                        <a href="index.php" class="desktop">Hubert &amp; Faust</a>
+                        <a href="index.php" class="mobile">Hubert &amp; Faust</a>
+                    </h1>
+                    <div class="toggleDown">
+                        <div class="menu-toggle-btn">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="pull-right">
-                <ul class="flat topbar">
-                    <li><a href="index.php">Home</a></li>
-                    <li>
-                        <a class="down">Products
-                            <i class="xn xn-angle-down"></i>
-                        </a>
-                        <ul class="subMenu">
-                            <li>
-                                <a href="category.php">Category A</a>
-                            </li>
-                            <li>
-                                <a href="category.php">Category B</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="promo.php">Promo</a></li>
-                    <li><a href="special-order.php">Special Order</a></li>
-                    <li><a href="contact.php">Contact Us</a></li>
-                </ul>
+                <div class="pull-right">
+                    <ul class="flat topbar">
+                        <li><a href="index.php">Home</a></li>
+                        <li>
+                            <a class="down">Products
+                                <i class="xn xn-angle-down"></i>
+                            </a>
+                            <ul class="subMenu">
+                                <li>
+                                    <a href="category.php">Category A</a>
+                                </li>
+                                <li>
+                                    <a href="category.php">Category B</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="promo.php">Promo</a></li>
+                        <li><a href="special-order.php">Special Order</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
         <div class="toggleMenu">
