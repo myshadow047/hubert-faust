faed9a8ab0d22c5143711bb54dd29f98834f4d37
aliaraaab <muhammadali.alaydrus@icloud.com>
2015-08-15 03:37:23 +0700

    [EDIT] Edit html & css

--- application/default/views/web/about_us.php
+++ application/default/views/web/about_us.php
@@ -10,37 +10,5 @@
 	        </div>
 	    	<?php echo isset($about_us['about_us'])? $about_us['about_us'] : '' ?>
     	</div>
-
-    	<!-- <div class="row"> -->
-	        <!-- <h2>Who we are?</h2> -->
-	        <!-- <div class="span-3">
-	        	<div class="wrapper">
-		        	<p>
-		        		Hubert and faust is an international furniture brand that emphasizes of the quality and luxury for our products. Hubert and Faust furniture collections has been combined with the design derived from various parts of the world , so it becomes Hubert and Faust advantages. 
-		        	</p>
-	        	</div>
-	        </div>
-	        <div class="span-3">
-	        	<div class="wrapper">
-		        	<p>
-						Every furniture collection that we present to the customers is always bring the pride for who owns it. It makes your house guests can enjoy the luxury and comfort from Hubert and Faust products.
-		        	</p>
-	        	</div>
-	        </div>
-	        <div class="span-3">
-	        	<div class="wrapper">
-		        	<p>
-						Hubert and faust will become the leader in making innovation and creativity, so we are ready to be at the forefront to fulfill our customers needs and dreams. We are also very open to consumers who are willing to provide input on us. Another advantage that we present to you is we invite you to join with us for making the imagination together and we will make your dream furniture come true.  Because Hubert and Faust provide this service theme “just customize your furniture as you are" where we would be very pleased to be able to realize the dream of furniture as you wish.
-		        	</p>
-	        	</div>
-	        </div>
-	        <div class="span-3">
-	        	<div class="wrapper">
-		        	<p>
-						We are committed to provide our best for our customers, because customers satisfaction is our happiness.
-		        	</p>
-	        	</div>
-	        </div> -->
-        <!-- </div> -->
     </div>
 </article>
